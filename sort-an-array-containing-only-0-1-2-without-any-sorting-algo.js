626a1a0f581adbf2ba768c87ff468abed3aa977e
Sahil Ahlawat <sahilahlawat2@gmail.com>
2022-04-06 14:20:03 +0530

    Update sort-an-array-containing-only-0-1-2-without-any-sorting-algo.js

--- sort-an-array-containing-only-0-1-2-without-any-sorting-algo.js
+++ sort-an-array-containing-only-0-1-2-without-any-sorting-algo.js
@@ -1,34 +1,20 @@
-let arr= [0,2,1];//201,021
-// have to sort this without any sorting also
-// my idea is to pick 0 and put it before or after 021
-// if first encounter if 1, put 0 before 1 else put 0 after 1
-if(arr[0] === 0){
-  if(arr[1] !== 1 )
-  {
-    let buffer = arr[1];
-    arr[1] = arr[2];
-    arr[2] = buffer;
+let a = [0,0,1,2,0,1,1,0,2,2,0,0,1,1,2,1,0];
+let zarr = [];
+let oarr = [];
+let tarr = [];
+for(let i = 1; i < a.length; i++){
+  
+    
+  if(a[i] === 0 ){
+    
+   zarr.push(a[i]);
   }
-}
-else if(arr[0] === 1){
-  if(arr[1] === 2 )
-  {
-    let buffer = arr[0];
-    arr[0] = arr[2];
-    arr[2] = buffer;
+  else if(a[i] === 1){
+     oarr.push(a[i])
   }
   else{
-    let buffer = arr[0];
-    arr[0] = arr[1];
-    arr[1] = buffer;
+    tarr.push(a[i])
   }
 }
-else{
-  if(arr[1] !== 1 )
-  {
-    let buffer = arr[1];
-    arr[1] = arr[2];
-    arr[2] = buffer;
-  }
-}
-console.log(arr)
+let finalarr = [...zarr,...oarr,...tarr];
+console.log(finalarr);
